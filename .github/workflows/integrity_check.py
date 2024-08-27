37e5abedac3a6c4afc92ba0252563116fa20060f
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2024-08-27 17:35:20 +0200

    Initial commit

---
+++ .github/workflows/integrity_check.py
@@ -0,0 +1,21 @@
+import json
+import os
+import logging
+import yaml
+from jsonschema import validators, validate
+
+with open("schema.yml", "r") as f:
+    data = yaml.safe_load(f)
+schema_path = data['schemas'][0]['path']
+
+with open(schema_path, "r") as f:
+    schema_data = json.load(f)
+validators.validator_for(schema_data).check_schema(schema_data)
+
+if "exemple-valide.json" in os.listdir():
+    with open("exemple-valide.json", "r") as f:
+        data = json.load(f)
+    validate(instance=data, schema=schema_data)
+
+else:
+    logging.warning('No example file to validate, consider adding one')
